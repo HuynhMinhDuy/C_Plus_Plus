d3b8786579b330010b32fceaf7c7de9293f03d89
HuynhMinhDuy <79966404+HuynhMinhDuy@users.noreply.github.com>
2023-04-30 18:01:33 +0700

    Create min_heap.cpp

---
+++ min_heap.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+
+	priority_queue<int, std::vector<int>, std::greater<int>> pq;
+	
+	pq.push(3);
+	pq.push(1);
+	pq.push(7);
+
+	cout << pq.top() << endl; // Output : 1
+
+	pq.pop();
+
+	cout << pq.top() << endl; // Output : 3
+
+
+	cout << "size : " << pq.size() << endl; // Output : 2 
+
+	while (!pq.empty()) {
+		cout << " e : " << pq.top() << endl;
+		pq.pop();
+	}
+
+	return 0;
+}
